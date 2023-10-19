f1c61415b0319c9088be8e1b262b0e4924298dd2
Flynn <517758840@qq.com>
2023-10-19 16:16:10 +0800

    Update main.dart
    用户状态的改变

--- flutter/day01/lib/main.dart
+++ flutter/day01/lib/main.dart
@@ -3,14 +3,46 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            title: Text("FLutter"),
+            title: const Text("Flutter"),
           ),
           body: Center(
-            child: Text("hello Flutter",style: TextStyle(fontSize: 18),),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                LZLHelloFlutter(false),
+                // const SizedBox(width: 24,),
+                const Text("hello Flutter",style: TextStyle(fontSize: 18),),
+              ],
+            ),
           ),
         ),
       )
   );
 }
+
+class LZLHelloFlutter extends StatefulWidget {
+
+  var stateCheckbox = false;
+  LZLHelloFlutter(this.stateCheckbox,{super.key});
+
+  @override
+  State<LZLHelloFlutter> createState() => _LZLHelloFlutterState();
+}
+
+class _LZLHelloFlutterState extends State<LZLHelloFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        value: widget.stateCheckbox,
+        onChanged: (value) {
+          setState(() {
+            widget.stateCheckbox = value ?? false;
+          });
+        }
+    );
+  }
+}
+
