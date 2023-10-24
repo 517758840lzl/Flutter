d0e1fb1a5587ab3280e8f702ea28099e37d1e989
Flynn <517758840@qq.com>
2023-10-24 15:40:00 +0800

    Interaptor

--- flutter/day01/lib/network/NetWorkTool.dart
+++ flutter/day01/lib/network/NetWorkTool.dart
@@ -16,6 +16,32 @@ class RequestManage {
     Interceptor? inter
   }) async {
     final dio = Dio(baseOptions);
+
+    Interceptor dInter = InterceptorsWrapper(
+      onRequest: (RequestOptions requestOptions, RequestInterceptorHandler handler) {
+        print("请求拦截 = ${requestOptions.uri}");
+        // requestOptions.baseUrl = "https://pub.dev";
+        handler.next(requestOptions);
+      },
+      onResponse: (Response response,ResponseInterceptorHandler handler) {
+        print("相应拦截 = $response");
+        handler.next(response);
+      },
+      onError: (DioError err, ErrorInterceptorHandler handler) {
+        print("失败拦截 = $err");
+        handler.next(err);
+      },
+    );
+
+    List<Interceptor> inters = [dInter];
+
+    // 请求单独拦截器
+    if (inter != null) {
+      inters.add(inter);
+    }
+
+    dio.interceptors.addAll(inters);
+
     final options = Options(method: method);
     try {
       Response response = await dio.request(url,options: options,queryParameters: params);
